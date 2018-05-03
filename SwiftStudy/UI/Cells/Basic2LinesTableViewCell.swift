9018eeb9ed64aabe2ecfda46ceffc40211ca7140
EisenKim <daseisen@gmail.com>
2018-05-03 11:34:47 +0900

    3

---
+++ SwiftStudy/UI/Cells/Basic2LinesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Basic2LinesTableViewCell.swift
+//  SwiftStudy
+//
+//  Created by Myeong chul Kim on 2018. 5. 1..
+//  Copyright © 2018년 Myeong chul Kim. All rights reserved.
+//
+
+import UIKit
+
+class Basic2LinesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
